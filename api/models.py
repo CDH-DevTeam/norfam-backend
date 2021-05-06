38a166f407876239a67d5d2bef624a49f76f1966
Arild Matsson <arild.matsson@gu.se>
2021-05-06 19:44:51 +0200

    Model doc comments

--- api/models.py
+++ api/models.py
@@ -9,9 +9,14 @@ from django.db import models
 
 
 class Term(models.Model):
+    """A unique word occurring in documents (type, not token, can be numeric)."""
+
     term_id = models.AutoField(primary_key=True)
+    # The word in lowercase
     term_term = models.CharField(unique=True, max_length=100)
+    # Not used?
     term_stem = models.CharField(max_length=100, blank=True, null=True)
+    # Document frequency, how many documents contain the word at least once
     term_df = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -19,9 +24,14 @@ class Term(models.Model):
         db_table = 'term'
 
 class DocTerm(models.Model):
+    """Mapping of a certain term in a certain document."""
+
     doc_term_id = models.AutoField(primary_key=True)
+    # The document where it occurs at least once
     doc_id = models.ForeignKey('Document', on_delete=models.PROTECT, db_column='doc_id', related_name='doc_terms')
+    # The term
     term = models.ForeignKey('Term', on_delete=models.PROTECT, db_column='term_id')
+    # Term frequency, how many times the term occurs in the document
     tf = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -29,10 +39,16 @@ class DocTerm(models.Model):
         db_table = 'doc_term'
 
 class Document(models.Model):
+    """An encyclopedic article on a topic."""
+
     doc_id = models.AutoField(primary_key=True)
+    # The topic, one or a few words
     doc_keyword = models.CharField(max_length=100)
+    # Full description, may contain HTML tags
     doc_text = models.TextField()
+    # Truncated description, max ca. 700 chars, possibly no incomplete HTML tags
     doc_abstr = models.CharField(max_length=2048)
+    # Running number unique among documents with same keyword, starting from 0
     doc_suppl = models.IntegerField(blank=True, null=True)
 
     class Meta:
@@ -40,9 +56,16 @@ class Document(models.Model):
         db_table = 'document'
 
 class Termsim(models.Model):
+    """Similarity of two terms.
+
+    Each term pair occurs twice – once in each direction."""
+
     termsim_id = models.AutoField(primary_key=True)
+    # Term one
     target = models.ForeignKey('Term', on_delete=models.PROTECT, db_column='term1_id', related_name='neighbors')
+    # Term two
     term = models.ForeignKey('Term', on_delete=models.PROTECT, db_column='term2_id')
+    # Similarity between 0 and 1
     similarity = models.FloatField()
 
     class Meta:
